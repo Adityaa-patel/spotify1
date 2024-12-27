204adb2dbbf5e3f94ff377881aa0ef043210331a
Aditya Patel <madeforaditya@gmail.com>
2024-12-28 03:46:24 +0530

    tried using panda instead

--- panda.py
+++ panda.py
@@ -1,11 +1,15 @@
+import pandas
 import json
+import time
 
-def data_extract(content_):
-    extracted_data = []
+start=time.time()
+
+df = pandas.DataFrame(columns=('song', 'artist',  'album', 'time', 'duration', 'skipped', 'reasonstart'))
+def data_extractv2(content_):
     for i in content_:
         if i['master_metadata_track_name'] is not None:
-            extracted_data.append([i["master_metadata_track_name"],i["master_metadata_album_artist_name"],i["master_metadata_album_album_name"],i["spotify_track_uri"][14:],i['ts'][:10],i['ms_played'],i["skipped"],i['reason_start']])
-    return extracted_data
+            df.loc[i["spotify_track_uri"][14:]] = [i["master_metadata_track_name"]]+[i["master_metadata_album_artist_name"]]+[i["master_metadata_album_album_name"]]+[i['ts'][:10]]+[i['ms_played']]+[i["skipped"]]+[i['reason_start']]
+    return df
 
 with open(r'_testfiles\Streaming_History_Audio_2021-2023_0.json', 'r', encoding='utf-8') as file:
     content1 = json.load(file) # content is in list format
@@ -17,5 +21,9 @@ with open(r'_testfiles\Streaming_History_Audio_2024_3.json', 'r', encoding='utf-
     content4 = json.load(file)
 with open(r'_testfiles\Streaming_History_Audio_2024_4.json', 'r', encoding='utf-8') as file:
     content5 = json.load(file)
-songsss=data_extract(content1)#+data_extract(content2) + data_extract(content3)+data_extract(content4)+data_extract(content5)
+songss=data_extractv2(content1)+data_extractv2(content2) + data_extractv2(content3)+data_extractv2(content4)+data_extractv2(content5)
 
+timetaken=time.time()-start
+
+print(songss)
+print(timetaken)
