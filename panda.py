64e9660607f9466e62a29471224ec2d577cda95b
Aditya Patel <madeforaditya@gmail.com>
2024-12-30 04:49:35 +0530

    panda

--- panda.py
+++ panda.py
@@ -1,31 +1,20 @@
-import pandas
-import json
-import time
+import pandas as pd
+import glob
+import os
 
-start=time.time()
+# Get the directory where the script is located
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# print(script_dir)
+#Construct the relative path to the _testfiles directory
+relative_path = os.path.join('c:\githubb\.venv\spotify1', '_testfiles', '*.json')
 
-df = pandas.DataFrame(columns=('song', 'artist',  'album', 'time', 'duration', 'skipped', 'reasonstart'))
-def data_extractv2(content_):
-    for i in content_:
-        if i['master_metadata_track_name'] is not None:
-            df.loc[i["spotify_track_uri"][14:]] = [i["master_metadata_track_name"]]+[i["master_metadata_album_artist_name"]]+[i["master_metadata_album_album_name"]]+[i['ts'][:10]]+[i['ms_played']]+[i["skipped"]]+[i['reason_start']]
-    return df
+files = glob.glob(os.path.join('c:\githubb\.venv\spotify1', '_testfiles', '*.json'))
 
-with open(r'_testfiles\Streaming_History_Audio_2021-2023_0.json', 'r', encoding='utf-8') as file:
-    content1 = json.load(file) # content is in list format
-with open(r'_testfiles\Streaming_History_Audio_2023_1.json', 'r', encoding='utf-8') as file:
-    content2 = json.load(file)
-with open(r'_testfiles\Streaming_History_Audio_2023-2024_2.json', 'r', encoding='utf-8') as file:
-    content3 = json.load(file)
-with open(r'_testfiles\Streaming_History_Audio_2024_3.json', 'r', encoding='utf-8') as file:
-    content4 = json.load(file)
-with open(r'_testfiles\Streaming_History_Audio_2024_4.json', 'r', encoding='utf-8') as file:
-    content5 = json.load(file)
-songss=data_extractv2(content1)+data_extractv2(content2) + data_extractv2(content3)+data_extractv2(content4)+data_extractv2(content5)
+if not files:
+    print(f"No JSON files found in the specified directory: {relative_path}")
+else:
+    df = pd.concat(map(pd.read_json, files))
+df=df[['ts','ms_played','master_metadata_track_name','master_metadata_album_artist_name','master_metadata_album_album_name','spotify_track_uri','reason_start','reason_end','offline_timestamp']]
 
-
-
-timetaken=time.time()-start
-
-print(songss)
-print(timetaken)
+songs_only_df = df.query('master_metadata_track_name != None')
+print(songs_only_df.dtypes)
