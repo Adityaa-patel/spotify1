92c92ea3f7d18b84bd05c609901c8467168a5e29
Aditya Patel <madeforaditya@gmail.com>
2025-01-09 02:25:26 +0530

    total now works

--- panda.py
+++ panda.py
@@ -15,3 +15,5 @@ else:
 df=df[['ts','ms_played','master_metadata_track_name','master_metadata_album_artist_name','master_metadata_album_album_name','spotify_track_uri','reason_start','reason_end','offline_timestamp']]
 df['ts'] = pd.to_datetime(df['ts'])
 songs_only_df = df.query('master_metadata_track_name != None')
+
+
