fbf64b7a50c5e43c339b84df4551f4ef722c4505
Aditya Patel <madeforaditya@gmail.com>
2025-01-17 01:22:55 +0530

    using spotify api

--- panda.py
+++ panda.py
@@ -2,6 +2,15 @@ import pandas as pd
 import glob
 import os
 import time
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Authenticate with Spotify API
+spotify = Spotify(auth_manager=SpotifyClientCredentials(client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+                                                         client_secret=os.getenv("SPOTIPY_CLIENT_SECRET")))
 
 time_start = time.time()
 relative_path = os.path.join('c:\githubb\.venv\spotify1', '_testfiles', '*.json')
@@ -14,6 +23,29 @@ else:
     df = pd.concat(map(pd.read_json, files))
 df=df[['ts','ms_played','master_metadata_track_name','master_metadata_album_artist_name','master_metadata_album_album_name','spotify_track_uri','reason_start','reason_end','offline_timestamp']]
 df['ts'] = pd.to_datetime(df['ts'])
-songs_only_df = df.query('master_metadata_track_name != None')
+songs_only_df = df.query('master_metadata_track_name != None and ms_played>1000')
 
+output_file = 'c:\\githubb\\.venv\\spotify1\\output.csv'
+songs_only_df.to_csv(output_file, index=False)
+print(f"DataFrame saved to")
+
+
+# def get_artist_genres(artist_name):
+#     try:
+#         results = spotify.search(q=f'artist:{artist_name}', type='artist', limit=1)
+#         if results['artists']['items']:
+#             return results['artists']['items'][0]['genres']
+#         else:
+#             return None
+#     except Exception as e:
+#         print(f"Error fetching genres for {artist_name}: {e}")
+#         return None
+
+# # Add genres column
+# songs_only_df['genres'] = songs_only_df['master_metadata_album_artist_name'].apply(get_artist_genres)
+
+# # Save to CSV
+# output_file = 'c:\\githubb\\.venv\\spotify1\\output.csv'
+# songs_only_df.to_csv(output_file, index=False)
+# print(f"DataFrame with genres saved to {output_file}")
 
